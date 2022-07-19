c7faf2daca9b3cec775af543e83b0a5ad3ad1579
Alex Martens <alex@thinglab.org>
2022-07-19 16:23:07 -0700

    module: change PSU link from Requires to BindsTo

--- module.nix
+++ module.nix
@@ -67,7 +67,7 @@ in
       {
         wantedBy = [ "multi-user.target" ];
         after = [ "network-online.target" "dev-psu.device" ];
-        requires = [ "dev-psu.device" ];
+        bindsTo = [ "dev-psu.device" ];
         description = "Corsair Mi MQTT";
         unitConfig.ReloadPropagatedFrom = "dev-psu.device";
         serviceConfig = {
